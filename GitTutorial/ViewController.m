6c4786acefbc7b7ba09d75ec8360687a7e48a8f4
Remy <ramesh.ramanaiah@ideabytes.com>
2017-02-02 16:25:58 +0530

    Animation 360 degree

--- GitTutorial/ViewController.m
+++ GitTutorial/ViewController.m
@@ -7,8 +7,11 @@
 //
 
 #import "ViewController.h"
+#import <QuartzCore/QuartzCore.h>
 
+#define M_PI   3.14159265358979323846264338327950288
 @interface ViewController ()
+@property (weak, nonatomic) IBOutlet UIImageView *myImage;
 
 - (IBAction)buttonTapped:(id)sender;
 @end
@@ -17,6 +20,17 @@
 
 - (void)viewDidLoad {
     [super viewDidLoad];
+//    [UIView animateWithDuration:10.0
+//delay:0.0
+//options:0
+//animations:^{
+//    _myImage.transform = CGAffineTransformMakeRotation(M_PI);
+//}
+//completion:^(BOOL finished){
+//    NSLog(@"Done!");
+//}];
+    
+    [self runSpinAnimationOnView:_myImage duration:1.0 rotations:1 repeat:5];
     // Do any additional setup after loading the view, typically from a nib.
 }
 
@@ -28,6 +42,31 @@
 - (IBAction)buttonTapped:(id)sender {
     NSLog(@"hi");
     NSLog(@"hi");
+    
+//    for(int i=0;i<100;i++){
+//
+//    //set point of rotation
+////    _myImage.center = CGPointMake(100.0, 100.0);
+//    
+//    //rotate rect
+//    _myImage.transform = CGAffineTransformMakeRotation(M_PI/2);
+//    
+//    }
+    
+}
 
+- (void) runSpinAnimationOnView:(UIView*)view duration:(CGFloat)duration rotations:(CGFloat)rotations repeat:(float)repeat
+{
+
+    CABasicAnimation* rotationAnimation;
+    rotationAnimation = [CABasicAnimation animationWithKeyPath:@"transform.rotation.z"];
+//    _myImage.layer.transform = CATransform3DMakeRotation(-M_PI*2, 0, 0, 1);
+
+    rotationAnimation.toValue = [NSNumber numberWithFloat: M_PI * 2.0 /* full rotation*/ * rotations * duration ];
+    rotationAnimation.duration = duration;
+    rotationAnimation.cumulative = YES;
+    rotationAnimation.repeatCount = repeat;
+    
+    [view.layer addAnimation:rotationAnimation forKey:@"rotationAnimation"];
 }
 @end
