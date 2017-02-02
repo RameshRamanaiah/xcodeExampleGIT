50c0b2ea18794808f6338ded219b25bb52a96152
nishanth9493 <nishanthkumar.ns@gmail.com>
2017-02-02 14:22:50 +0530

    Initial Commit

---
+++ GitTutorial/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  GitTutorial
+//
+//  Created by Ideabytes on 02/02/17.
+//  Copyright © 2017 Ideabytes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
---
+++ GitTutorial/main.m
@@ -0,0 +1,16 @@
+//
+//  main.m
+//  GitTutorial
+//
+//  Created by Ideabytes on 02/02/17.
+//  Copyright © 2017 Ideabytes. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+#import "AppDelegate.h"
+
+int main(int argc, char * argv[]) {
+    @autoreleasepool {
+        return UIApplicationMain(argc, argv, nil, NSStringFromClass([AppDelegate class]));
+    }
+}
